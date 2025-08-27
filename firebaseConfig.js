33cd8809755a32cf9e14be67b3654c1c4c5ca9ec
cheeets <14teenfour.fourten@gmail.comm>
2025-08-27 15:34:25 +0800

    Firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVMmWjym_p59Nzi5IUxxWNy5vz1osrtN0",
+  authDomain: "cll-goals.firebaseapp.com",
+  projectId: "cll-goals",
+  storageBucket: "cll-goals.firebasestorage.app",
+  messagingSenderId: "1031250055258",
+  appId: "1:1031250055258:web:993ff8996632024990c863"
+};
+
+
+const app = initializeApp(firebaseConfig);
