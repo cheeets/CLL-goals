38e2201646c429b73cd18ea61ea8762e97b6257c
cheeets <14teenfour.fourten@gmail.comm>
2025-09-03 15:50:20 +0800

    Create goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,15 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+//const firebaseConfig = {
+  //apiKey: "AIzaSyBVMmWjym_p59Nzi5IUxxWNy5vz1osrtN0",
+  //authDomain: "cll-goals.firebaseapp.com",
+  //projectId: "cll-goals",
+  //storageBucket: "cll-goals.firebasestorage.app",
+  //messagingSenderId: "1031250055258",
+  //appId: "1:1031250055258:web:993ff8996632024990c863"
+//};
 const firebaseConfig = {
-  apiKey: "AIzaSyBVMmWjym_p59Nzi5IUxxWNy5vz1osrtN0",
-  authDomain: "cll-goals.firebaseapp.com",
-  projectId: "cll-goals",
-  storageBucket: "cll-goals.firebasestorage.app",
-  messagingSenderId: "1031250055258",
-  appId: "1:1031250055258:web:993ff8996632024990c863"
+  apiKey: 'AIzaSyDaHxZohIvyN13cEcBI95P4PKZAmcghGxw',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'edutasker-31495',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
