b85adf89ec9b766dd876daf5fc9f5689bb0c73c9
cheeets <14teenfour.fourten@gmail.comm>
2025-09-03 15:34:06 +0800

    Create goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBVMmWjym_p59Nzi5IUxxWNy5vz1osrtN0",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
